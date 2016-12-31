7006b07862a531c8a7f2c79543e47ee4d0aa8a4a
Brice <brice.peters@laposte.net>
2016-12-31 17:41:07 +0100

    quasi finis

---
+++ src/game/ia/utc/Node.java
@@ -0,0 +1,216 @@
+package game.ia;
+
+import game.model.Puissance4;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Classe de création des arêtes pour les graphes
+ *
+ */
+
+public class Edge {
+
+    private Edge pere;
+    private ArrayList<Edge> filsListe;
+    private int nbWin;
+    private int nbSimulation;
+    private Puissance4 state;
+    private boolean ia ;
+    private int mouvement;
+
+   
+
+   public Edge(Puissance4 newState) {
+       state = new Puissance4(newState);
+       nbWin = 0;
+       nbSimulation = 0;
+       ia = false;
+       filsListe = new ArrayList<>();
+   }
+
+    public Edge(int newMouvement,Edge newPere,Puissance4 newState) {
+        pere = newPere;
+        state = new Puissance4(newState);
+        nbWin = 0;
+        nbSimulation = 0;
+        ia = !pere.isIa();
+        this.mouvement = newMouvement;
+        filsListe = new ArrayList<>();
+    }
+
+
+   public int simulation (){
+       Random rand = new Random();
+       int move;
+       Puissance4 copie = new Puissance4(state);
+       copie.end();
+       while(copie.getEnd() == Puissance4.End.NO){
+           move = rand.nextInt(7);
+           if(copie.mouvementValide(move)) {
+               copie.mouvement(move);
+               copie.end();
+           }
+       }
+       if(copie.getEnd() == Puissance4.End.DRAW || copie.getEnd() == Puissance4.End.P1) {
+
+           return 0;
+
+       }else{
+           return 1;
+       }
+   }
+
+    public int simulationGagnante (){
+        Random rand = new Random();
+        int move;
+        int i =0;
+        Puissance4 copie = new Puissance4(state);
+        Puissance4 copie2 = new Puissance4(copie) ;
+        copie.end();
+        copie2.end();
+        if(copie.getEnd() == Puissance4.End.NO) {
+            while (copie2.getEnd() == Puissance4.End.NO && i < 7) {
+                copie2 = new Puissance4(copie);
+                if (copie2.mouvementValide(i)) {
+                    copie2.mouvement(i);
+                    copie2.end();
+                }
+                i++;
+            }
+
+            if (copie2.getEnd() == Puissance4.End.NO) {
+                while (copie.getEnd() == Puissance4.End.NO) {
+                    move = rand.nextInt(7);
+                    while (!copie.mouvementValide(move)) {
+                        move = rand.nextInt(7);
+                    }
+                    copie.mouvement(move);
+                    copie.end();
+                }
+            } else {
+                move = i - 1;
+                copie.mouvement(move);
+                copie.end();
+            }
+        }
+        if(copie.getEnd() == Puissance4.End.P2) {
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+
+   public void maj(int victoire){
+       this.nbSimulation++;
+       this.nbWin += victoire;
+       if(this.getPere()!= null)
+           getPere().maj(victoire);
+   }
+
+    public Edge getPere() {
+        return pere;
+    }
+
+    public int getNbSimulation(){
+        return nbSimulation;
+    }
+
+    public double getBeta(){
+        double beta;
+        if(getPere() == null){
+            beta = 1;
+        }else {
+            if (isIa()) {
+                beta = (double)(nbWin / nbSimulation) + Math.sqrt(2) * Math.sqrt(Math.log(getPere().getNbSimulation()) / Math.log(getNbSimulation()));
+            } else {
+                beta = -((double)(nbWin / nbSimulation) + Math.sqrt(2) * Math.sqrt(Math.log(getPere().getNbSimulation()) / Math.log(getNbSimulation())));
+            }
+        }
+            return beta;
+    }
+
+    public boolean isIa() {
+        return ia;
+    }
+
+    public int getBestMovement() {
+        double max = -1;
+        int bestMovement = 9;
+        for(Edge fils : filsListe){
+            if ((double)fils.nbWin/fils.nbSimulation > max){
+                max = (double)fils.nbWin/fils.nbSimulation;
+                bestMovement = fils.getMouvement();
+            }
+        }
+        System.out.println("NB Simulation = "+nbSimulation+"\nProba de victoire = "+ max);
+        return bestMovement;
+    }
+
+    public int getMouvement() {
+        return mouvement;
+    }
+
+    public Edge selection() {
+        this.state.end();
+        int filsPossible =0;
+        if(this.state.getEnd()!= Puissance4.End.NO) {
+            return this;
+        }else{
+            for(int i = 0; i < 7 ; i++){
+                if(state.mouvementValide(i)){
+                    filsPossible++;
+                }
+            }
+            if(this.filsListe.isEmpty()||this.filsListe.size() < filsPossible){
+                return this;
+            }else{
+                double max =this.getFils(0).getBeta();
+                int selection = 0;
+                for(int i = 0; i < filsListe.size();i++){
+                    if(this.getFils(i).getBeta()>max){
+                        max = this.getFils(i).getBeta();
+                        selection = i;
+                    }
+                }
+                return getFils(selection).selection();
+            }
+        }
+    }
+
+    private Edge getFils(int i) {
+        return filsListe.get(i);
+    }
+
+    public Edge developpe() {
+        this.state.end();
+        if(this.state.getEnd()== Puissance4.End.NO){
+
+            ArrayList<Integer> moveList = new ArrayList<>();
+            for (int i = 0; i < 7; i ++){
+                moveList.add(i);
+            }
+            for(Edge e : filsListe){
+                moveList.remove((Object)e.getMouvement());
+            }
+
+            Random rand = new Random();
+            int move = moveList.get(rand.nextInt(moveList.size()));
+
+            Puissance4 copie = new Puissance4(this.state);
+
+            while(!copie.mouvementValide(move)){
+                moveList.remove((Object)move);
+                move = moveList.get(rand.nextInt(moveList.size()));
+            }
+            copie.mouvement(move);
+
+            Edge newFils = new Edge(move,this,copie);
+            this.filsListe.add(newFils);
+            return newFils;
+        }else{
+            return this;
+        }
+    }
+}
